1a0980197718101e6b6d8cbf52912fc258eef2d2
Justin Richards <justin.richards@msnst.com>
2014-07-26 21:07:29 -0500

    Indenting properly

--- patterngrid.h
+++ patterngrid.h
@@ -17,74 +17,74 @@ class PatternGrid : public QQuickItem
 
   int m_columns;
 
-int m_cellWidth;
+  int m_cellWidth;
 
-int m_cellHeight;
+  int m_cellHeight;
 
 public:
   explicit PatternGrid(QQuickItem *parent = 0);
   void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
 
-int rows() const
-{
-  return m_rows;
-}
+  int rows() const
+  {
+    return m_rows;
+  }
 
-int columns() const
-{
-  return m_columns;
-}
+  int columns() const
+  {
+    return m_columns;
+  }
 
-int cellWidth() const
-{
-  return m_cellWidth;
-}
+  int cellWidth() const
+  {
+    return m_cellWidth;
+  }
 
-int cellHeight() const
-{
-  return m_cellHeight;
-}
+  int cellHeight() const
+  {
+    return m_cellHeight;
+  }
 
 signals:
 
-void rowsChanged(int arg);
+  void rowsChanged(int arg);
 
-void columnsChanged(int arg);
+  void columnsChanged(int arg);
 
-void cellWidthChanged(int arg);
+  void cellWidthChanged(int arg);
 
-void cellHeightChanged(int arg);
+  void cellHeightChanged(int arg);
 
 public slots:
 
-void setRows(int arg)
-{
-  if (m_rows != arg) {
-    m_rows = arg;
-    emit rowsChanged(arg);
+  void setRows(int arg)
+  {
+    if (m_rows != arg) {
+      m_rows = arg;
+      emit rowsChanged(arg);
+    }
   }
-}
-void setColumns(int arg)
-{
-  if (m_columns != arg) {
-    m_columns = arg;
-    emit columnsChanged(arg);
+  void setColumns(int arg)
+  {
+    if (m_columns != arg) {
+      m_columns = arg;
+      emit columnsChanged(arg);
+    }
   }
-}
-void setCellWidth(int arg)
-{
-  if (m_cellWidth != arg) {
-    m_cellWidth = arg;
-    emit cellWidthChanged(arg);
+  void setCellWidth(int arg)
+  {
+    if (m_cellWidth != arg) {
+      m_cellWidth = arg;
+      emit cellWidthChanged(arg);
+    }
   }
-}
-void setCellHeight(int arg)
-{
-  if (m_cellHeight != arg) {
-    m_cellHeight = arg;
-    emit cellHeightChanged(arg);
+  void setCellHeight(int arg)
+  {
+    if (m_cellHeight != arg) {
+      m_cellHeight = arg;
+      emit cellHeightChanged(arg);
+    }
   }
-}
 };
 
 QML_DECLARE_TYPE(PatternGrid)
