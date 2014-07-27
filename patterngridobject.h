9915e2a01e9c3e6f65dbb581f713eca064daffe2
Justin Richards <justin.richards@msnst.com>
2014-07-27 13:22:44 -0500

    Filled in code for patterngridobject

--- patterngridobject.h
+++ patterngridobject.h
@@ -2,12 +2,35 @@
 #define PATTERNGRIDOBJECT_H
 
 #include <QObject>
+#include <QRect>
 
 class PatternGridObject : public QObject
 {
   Q_OBJECT
 public:
   explicit PatternGridObject(QObject *parent = 0);
+
+  QRect getRect() const;
+  void setRect(const QRect &value);
+
+  int getColumn() const;
+  void setColumn(int value);
+
+  int getRow() const;
+  void setRow(int value);
+
+  QColor getColor() const;
+  void setColor(const QColor &value);
+
+  int getSymbolNumber() const;
+  void setSymbolNumber(int value);
+
+private:
+  QRect rect;
+  int column;
+  int row;
+  QColor color;
+  int symbolNumber;
 
 signals:
 
